7f2df8ad60680d84bf6d3be92f0a718539a75f5d
duckboycool <duckboycool101@gmail.com>
2021-04-22 16:02:19 -0600

    Owner utility commands

--- commands.py
+++ commands.py
@@ -6,7 +6,9 @@ Loads in other cogs and manages cog reloading.
 import os
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+
+embed_color = 0x59a2a1
 
 def update_status(bot, games={}):
     if len(games) == 0:
@@ -27,26 +29,69 @@ class SecHitComms(commands.Cog):
 
         await update_status(self.bot)
 
+    @tasks.loop(minutes=30.0)
+    async def status_reload(self): #Update status periodically
+        await update_status(self.bot)
+
     @commands.command()
     @commands.is_owner()
-    async def reload(self, ctx):
+    async def reload(self, ctx, cogName=None):
         print("Reloading Cogs")
 
         game = self.bot.get_cog('GameCommands')
 
         games = game.games
 
-        for cog in os.listdir('./cogs'):
-            if cog.startswith('cog_'):
-                print(f"Reloading cog {os.path.splitext(cog)[0]}")
+        if cogName:
+            if f'cog_{cogName}.py' in os.listdir('./cogs'):
+                print(f'Reloading cog {cogName}')
 
-                self.bot.reload_extension(f'cogs.{os.path.splitext(cog)[0]}')
-        
+                self.bot.reload_extension(f'cogs.cog_{cogName}')
+
+            else:
+                await ctx.send(f'Did not find cog {cogName}.')
+                return
+
+        else:
+            for cog in os.listdir('./cogs'):
+                if cog.startswith('cog_'):
+                    print(f'Reloading cog {os.path.splitext(cog)[0]}')
+
+                    self.bot.reload_extension(f'cogs.{os.path.splitext(cog)[0]}')
+
+        if not(cogName) or cogName == 'games':
+            game = self.bot.get_cog('GameCommands')
+
+            game._load(games)
+
+        await ctx.send("Reloaded sucessfully.\n")
+
+    @commands.command()
+    @commands.is_owner()
+    async def gamestatus(self, ctx):
         game = self.bot.get_cog('GameCommands')
 
-        game._load(games)
+        games = game.games
 
-        await ctx.send("Reloaded sucessfully.")
+        await ctx.send(({game.code: (game.started, len(game.players)) for game in games.values()}))
+
+    @commands.command()
+    @commands.is_owner()
+    async def warn(self, ctx, minutes=None):
+        Game = self.bot.get_cog('GameCommands')
+
+        embed = discord.Embed(
+            title='Info',
+            description=f"The bot will be restarted in {minutes if minutes else 'a few'} minutes. If you are playing a started game by then, the lobby will reset.",
+            color=embed_color
+        )
+
+        embed.set_thumbnail(url='https://cdn.discordapp.com/avatars/691007847416201217/31b3c53065bdb536bc9d59d73f14a202.png?size=256')
+
+        for game in Game.games:
+            for player in Game.games[game].players:
+                await player.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(SecHitComms(bot))
